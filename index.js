324940f295b10eb9875bc9fd410898d8518336e0
ajaydhungel23 <91951582+ajaydhungel23@users.noreply.github.com>
2022-06-16 11:00:26 +0545

    UPdate index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! from AWS Proton');
+  res.send('Message from AWS Proton');
 });
 app.listen(80, function () {
   console.log('Demo NodeJS app listening on port 80.');
